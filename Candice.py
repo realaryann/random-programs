578f1c58426b2c3fd1e3627f5ae4b0749b8f0fe1
Aryan Nagpal <70107111+therealvortex9@users.noreply.github.com>
2020-10-13 17:12:22 +0530

    Update Candice.py

--- Candice.py
+++ Candice.py
@@ -8,6 +8,7 @@ print("[1] Mathematical Calculation")
 print("[2] Data Entry")
 print("[3] Guessing Game")
 print("[4] Number Converter")
+print("[5] Heads/Tails decider")
 
 
 def mat(x,y,z):
@@ -146,4 +147,31 @@ if a1 == 4:
     types = input("Enter the type you want to convert to: ").strip().capitalize()
     
     ans = conv(x,types)
-    print(ans)      
+    print(ans)
+    
+if a1 == 5:
+    choices = ["Heads","Tails"]
+
+    def rev(a,b,c,d):
+        select = random.choice(choices)
+        if b == select and d != select:
+            result1 = (a, "has won")
+            return result1
+        
+        elif d == select and b != select:
+            result2 = (c, "has won")
+            return result2
+
+    while True:
+        a = input("Enter p1  name: ").strip().capitalize()
+        b = input("Enter your choice: ").strip().capitalize()
+        c = input("Enter p2 name: ").strip().capitalize()
+        d = input("Enter your choice: ").strip().capitalize()
+        print(rev(a,b,c,d))
+        ask = input("Would you like to play again? ").strip().capitalize()
+        if ask == "Yes":
+            continue
+        elif ask == "No":
+            break
+    
+    print("Thanks for playing!")
