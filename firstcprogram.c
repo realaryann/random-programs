3ef5666e6a1170370745757f751b33649775c5d8
Aryan Nagpal <70107111+therealvortex9@users.noreply.github.com>
2022-04-06 14:10:12 +0530

    Create firstcprogram.c

---
+++ firstcprogram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char character[] = "Bruh";
+    int age = 50;
+    printf("There was once a man named %s",character);
+    return 0;
+
+}
